10ab2c75d9f8dcb7e118b299e864597de25533de
Stefan Zamfira <stefan.zamfira@osf-global.com>
2015-10-06 22:07:50 +0300

    responsive updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,20 @@ WebFontConfig = {
 (function($){
 
     // customize all selects
-    $('select').customSelect()
+    $('select').customSelect();
+
+    var $mobileSidebar = $('.sideMeta');
+    // mobile nav
+    $('.js-nav-trigger').on('click', function(e){
+        e.preventDefault();
+
+        $mobileSidebar.toggleClass('active');
+        var isActive = $mobileSidebar.hasClass('active');
+
+        $mobileSidebar.animate({
+            right : isActive ? 0 : '-50%'
+        });
+
+    });
 
 })(jQuery);
--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,6 +1,20 @@
 (function($){
 
     // customize all selects
-    $('select').customSelect()
+    $('select').customSelect();
+
+    var $mobileSidebar = $('.sideMeta');
+    // mobile nav
+    $('.js-nav-trigger').on('click', function(e){
+        e.preventDefault();
+
+        $mobileSidebar.toggleClass('active');
+        var isActive = $mobileSidebar.hasClass('active');
+
+        $mobileSidebar.animate({
+            right : isActive ? 0 : '-50%'
+        });
+
+    });
 
 })(jQuery);
